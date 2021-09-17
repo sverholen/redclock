5f31a5277989091f73868cfca90a25b7c3a7526b
Stijn Verholen <boro@inuits.eu>
2021-09-17 13:44:31 +0200

    Added gui handling

---
+++ app.py
@@ -0,0 +1,11 @@
+import sys
+
+from PyQt5.QtWidgets import (
+    QApplication, QDialog, QMainWindow, QMessageBox
+)
+from PyQt5.uic import loadUi
+from redclock_ui import Ui_MainWindow
+
+
+def Window(QMainWindow, UiMainWindow):
+    
